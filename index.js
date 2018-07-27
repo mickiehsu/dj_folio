900ef178d6429ca9a84e6a5fb707a215231d05bd
Mickie Hsu <mickie.hsu@appfolio.com>
2018-07-26 19:38:27 -0700

    mh - set up Express to point to “client/build” for static assets

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
