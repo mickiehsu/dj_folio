eb7e37901a18fd0a43a66b653160f94da5ab1616
Mickie Hsu <mickie.hsu@appfolio.com>
2018-07-27 03:14:32 -0700

    mh - resolve CORS problem

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ app.listen(port, () => console.log('Example app listening on port' + port))
   // app.use(express.static('client/build'));
 // }
 
+// CORS Problem
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://djfolio-demo.herokuapp.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 
 let currentSecond = 0;
 let video_queue = [];
