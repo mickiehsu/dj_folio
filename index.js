ad2dba4cdd653cb7e8e0aa923f78b1033b59185f
Mickie Hsu <mickie.hsu@appfolio.com>
2018-07-27 09:21:53 -0700

    mh/yc - fix timestamps bug

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 let currentSecond = 0;
 let video_queue = [];
 
-let intervalObj = setInterval(() => {
+setInterval(() => {
   if (video_queue.length > 0) {
     currentSecond = currentSecond + 1;
 
@@ -46,8 +46,6 @@ let intervalObj = setInterval(() => {
       currentSecond = 0;
       video_queue.shift();
     }
-  } else {
-    clearInterval(intervalObj);
   }
 }, 1000);
 
